82b2dbaca81a0b39c04776150b39dd1dcdb88df2
Rain zhou <zhouchengyuhun@outlook.com>
2024-07-10 15:39:12 +0800

    Can save one item.

--- functional_tests.py
+++ functional_tests.py
@@ -36,26 +36,34 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_element(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        # self.assertTrue(
+        #     any(row.text == '1:Buy peacock feathers' for row in rows),
+        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
-
+        self.fail('Finish the test!')
         # 她访问那个URL，发现她的待办事项列表还在
 
         # 她很满意，去睡觉了
