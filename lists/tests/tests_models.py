d9b6803f68b8078eeadfeede56613c9a43b44746
Rain zhou <zhouchengyuhun@outlook.com>
2024-07-14 22:18:37 +0800

    Use get_absolute_url on model to DRY urls in views

--- lists/tests/tests_models.py
+++ lists/tests/tests_models.py
@@ -31,4 +31,8 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
 
--- lists/views.py
+++ lists/views.py
@@ -17,11 +17,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
+
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_id}/')
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_id}/')
+        except ValidationError:
+            error = "You can't have an empty list item"
     items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'items': items, 'list': list_})
+    return render(request, 'list.html', {'items': items, 'list': list_, 'error': error})
 
 
 def new_list(request):
@@ -36,7 +43,7 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
 
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
 
 
 # def add_item(request, list_id):
